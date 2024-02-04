983f012dded78cbf7676375884a53c1cde31bc6f
Jethro_Block <jethroblock22@gmail.COM>
2024-02-04 14:31:27 +0000

    formatting

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,29 @@
-'use client';
+"use client";
 import { fetchWeather } from "@/utils/fetchWeather";
 import { useState } from "react";
 
 export default function Home() {
-const [weatherData, setWeatherData] = useState(null)
-const [errorMessage, setErrorMessage] = useState("")
+  const [weatherData, setWeatherData] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
 
-const handleSearch = async (city: string) => {
-  try {
-    const data = await fetchWeather(city);
-    setWeatherData(data)
-    console.log(data) // placeholder
-  } catch (err) {
-    console.error("Failed to fetch the weather: ", err)
-    if (err instanceof Error) {
-      setErrorMessage(err.message)
-    } else {
-      setErrorMessage("An unknown error occurred")
+  const handleSearch = async (city: string) => {
+    try {
+      const data = await fetchWeather(city);
+      setWeatherData(data);
+      console.log(data); // placeholder
+    } catch (err) {
+      console.error("Failed to fetch the weather: ", err);
+      if (err instanceof Error) {
+        setErrorMessage(err.message);
+      } else {
+        setErrorMessage("An unknown error occurred");
+      }
     }
-  }
-}
+  };
 
   return (
-<>
-
-<h1>Placeholder Header For Home</h1>
-
-</>
-
+    <>
+      <h1>Placeholder Header For Home</h1>
+    </>
   );
 }
