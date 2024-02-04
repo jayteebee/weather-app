485b9cdb3ca2f53eda7a6ae5023da6c009c467b5
Jethro_Block <jethroblock22@gmail.COM>
2024-02-04 15:51:04 +0000

    render search bar test

--- __tests__/SearchBar.test.tsx
+++ __tests__/SearchBar.test.tsx
@@ -1,1 +1,11 @@
 import SearchBar from "@/components/SearchBar";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom"
+
+describe("SearchBar Component", () => {
+    test("Renders correctly", () => {
+        render(<SearchBar onSearch={jest.fn()} />)
+        expect(screen.getByLabelText(/city/i)).toBeInTheDocument();
+        expect(screen.getByRole('button', {name: /search/i})).toBeInTheDocument();
+    })
+})
