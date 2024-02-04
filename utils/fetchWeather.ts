d25eb7c48805e65eab6c6174406ee0c6ba806ae7
Jethro_Block <jethroblock22@gmail.COM>
2024-02-04 14:21:51 +0000

    add placeholder error handling

--- utils/fetchWeather.ts
+++ utils/fetchWeather.ts
@@ -5,6 +5,6 @@ export const fetchWeather = async (city: string) => {
         const response = await axiosInstance.get("current.json", {params: {q: city}});
         return response.data;
     } catch (error) {
-
+        console.error('Error fetching weather data: City not found.');
     }
 }
