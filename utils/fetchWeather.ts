7c97e3e2b74302019a1ed8d2734722203898cb02
Jethro_Block <jethroblock22@gmail.COM>
2024-02-04 14:21:09 +0000

    add call and response

--- utils/fetchWeather.ts
+++ utils/fetchWeather.ts
@@ -1,3 +1,10 @@
 import axiosInstance from "./axiosInstance";
 
-export const fetchWeather = async (city: string) => {}
+export const fetchWeather = async (city: string) => {
+    try {
+        const response = await axiosInstance.get("current.json", {params: {q: city}});
+        return response.data;
+    } catch (error) {
+
+    }
+}
