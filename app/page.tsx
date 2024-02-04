1baebd4c89d3d04fdb115f3c031b39438ff99a7d
Jethro_Block <jethroblock22@gmail.COM>
2024-02-04 14:27:10 +0000

    add handleSearch with the fetchWeather call

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
+'use client';
+import { fetchWeather } from "@/utils/fetchWeather";
+import { useState } from "react";
+
 export default function Home() {
+const [weatherData, setWeatherData] = useState(null)
+
+const handleSearch = async (city: string) => {
+  try {
+    const data = await fetchWeather(city);
+    setWeatherData(data)
+    console.log(data) // placeholder
+  } catch (err) {
+    console.error(err)
+  }
+}
+
   return (
-    <div>
+
       <h1>Placeholder Header For Home</h1>
-    </div>
+
   );
 }
