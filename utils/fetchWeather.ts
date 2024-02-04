858989d187ec6fd71aca1753785e2dc0d0c5ef25
Jethro_Block <jethroblock22@gmail.COM>
2024-02-04 15:24:46 +0000

    add any type for the error

--- utils/fetchWeather.ts
+++ utils/fetchWeather.ts
@@ -4,7 +4,7 @@ export const fetchWeather = async (city: string) => {
     try {
         const response = await axiosInstance.get("current.json", {params: {q: city}});
         return response.data;
-    } catch (error) {
+    } catch (error: any) {
         if (error.response && error.response.data && error.response.data.error && error.response.data.error.code === 1006) {
             console.error('Error fetching weather data: City not found.');
             throw new Error('City not found. Please check spelling and try again.');
