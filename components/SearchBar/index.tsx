fcbeae050e0dd2e0718b9eefcd17d4d7e1555131
Jethro_Block <jethroblock22@gmail.COM>
2024-02-04 14:40:30 +0000

    handle user input

--- components/SearchBar/index.tsx
+++ components/SearchBar/index.tsx
@@ -1,15 +1,23 @@
 import { Box, Button, TextField } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 
 type SearchBarProps = {
   onSearch: (city: string) => void;
 };
 
 const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+    const [city, setCity] = useState("");
+
+const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    onSearch(city)
+}
 
     return (
-        <Box>
-            <TextField />
+        <Box component="form" onSubmit={handleSubmit} noValidate>
+            <TextField
+            value={city}
+            />
             <Button>
                 Search
             </Button>
