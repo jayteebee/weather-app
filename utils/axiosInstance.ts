42db4ba62ea09621b2b6451b63180ac4d224f1a8
Jethro_Block <jethroblock22@gmail.COM>
2024-02-04 14:18:54 +0000

    add url and add api key to params

--- utils/axiosInstance.ts
+++ utils/axiosInstance.ts
@@ -1,1 +1,10 @@
 import axios from 'axios';
+
+const axiosInstance = axios.create({
+    baseURL: "https://api.weatherapi.com/v1",
+    params: {
+        key: process.env.NEXT_PUBLIC_WEATHER_API_KEY
+    }
+});
+
+export default axiosInstance;
