b94cb82dfb3fc62b5b5fd34118c588585e6f5045
Jethro_Block <jethroblock22@gmail.COM>
2024-02-04 13:53:37 +0000

    populate theme

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,6 +1,37 @@
 'use client'
 import {createTheme} from '@mui/material/styles'
 
-const theme = createTheme({})
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#1976d2', // blue
+        },
+        secondary: {
+            main: '#ffc107', // yellow
+        },
+        background: {
+            default: '#e1f5fe', // light blue
+        },
+        text: {
+            primary: '#212121', // dark grey
+            secondary: '#757575', // lighter grey
+        },
+    },
+    typography: {
+        fontFamily: '"Roboto", "Helvetica", sans-serif',
+        h1: {
+            fontSize: '2.5rem',
+            fontWeight: 300,
+        },
+        h2: {
+            fontSize: '2rem',
+            fontWeight: 400,
+        },
+        h3: {
+            fontSize: '1.75rem',
+            fontWeight: 400,
+        },
+    },
+});
 
 export default theme;
